ab4fa565657735b6c0ea0689d01037485b3524d5
Prashanth Pratapagiri <prashanthp@cpwonlinesolutions.com>
2016-02-28 21:34:50 +0000

    new changes

---
+++ config/manifest.js
@@ -0,0 +1,60 @@
+var Config = require('config');
+var GoodConsole = require('good-console');
+var manifest = {
+    server: {
+        debug: {
+            request: ['error']
+        },
+        connections: {
+            routes: {
+                cors: Config.get('cors')
+            }
+        },
+        app: {
+            db: Config.get('db')
+        }
+    },
+    connections: [{
+        port: Config.get('port'),
+        host: Config.get('host')
+    }],
+    registrations: [
+        {
+            plugin: 'inert'
+        },
+        {
+            plugin: {
+                register: 'good',
+                options: {
+                    reporters: [{
+                        reporter: GoodConsole,
+                        events: {
+                            request: '*',
+                            error: '*',
+                            log: '*'
+                        }
+                    }]
+                }
+            }
+        },
+        {
+            plugin: 'halacious'
+        },
+        {
+            plugin: './lib/amma-db'
+        },
+        {
+            plugin: './lib/amma-db-parser'
+        },
+        {
+            plugin: './lib/amma-file'
+        },
+        {
+            plugin: './lib/amma-crud'
+        },
+        {
+            plugin: './lib/amma-user'
+        }
+    ]
+};
+module.exports = manifest;
